2b4959e6f1980c31d24a9a09ea37dba30d2e4dbd
ArianaAguado <ariana.aguado@educa.madrid.org>
2026-05-16 23:02:03 +0200

    commit inicial

---
+++ src/test/java/com/ariana/streamingapi/StreamingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ariana.streamingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
